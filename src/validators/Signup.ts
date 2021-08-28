1cc9ed63920d23c2fa980cf26b821dcae7148a86
d-takeuchi <kuronk.kuro3@gmail.com>
2021-08-29 01:29:22 +0900

    ユーザー新規登録処理の微修正

--- src/validators/Signup.ts
+++ src/validators/Signup.ts
@@ -4,5 +4,5 @@ import * as yup from "yup";
 export const signupSchema = yup.object().shape({
   userName: yup.string().required(),
   email: yup.string().email().required(),
-  password: yup.string().min(4).max(20).required(),
+  password: yup.string().min(6).max(20).required(),
 });
